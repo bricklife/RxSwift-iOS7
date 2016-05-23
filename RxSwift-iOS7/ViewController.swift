ce97c3a461fb8365878b01542b59ed6a1d678feb
Shinichiro Oba <ooba@bricklife.com>
2016-05-24 00:34:36 +0900

    Use RxSwift and RxCocoa

--- RxSwift-iOS7/ViewController.swift
+++ RxSwift-iOS7/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.rx_text.asDriver()
+            .map { "'\($0)'" }
+            .drive(label.rx_text)
+            .addDisposableTo(disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
